e3f2b04f3802cc3a51e8037832ec83e4ef0623fb
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-09 14:21:21 +0200

    Rewrite example test to demonstrate setUp/tearDown and groups

--- test/ExampleTest.js
+++ test/ExampleTest.js
@@ -1,4 +1,30 @@
-module.exports.testHelloWorld = function(test) {
-	test.equal(5, 5, 'Value should be 5');
-	test.done();
+module.exports = {
+	setUp: function(callback) {
+		console.log('setUp - Outer');
+		callback();
+	},
+	tearDown: function(callback) {
+		console.log('tearDown - Outer');
+		callback();
+	},
+	test1: function (test) {
+		console.log('test - Outer');
+		test.ok(5);
+		test.done();
+	},
+	group1: {
+		setUp: function(callback) {
+			console.log('setUp - Inner');
+			callback();
+		},
+		tearDown: function(callback) {
+			console.log('tearDown - Inner');
+			callback();
+		},
+		test1: function (test) {
+			console.log('test - Inner');
+			test.ok(5);
+			test.done();
+		},
+	}
 };
