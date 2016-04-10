6c86beeef716c3fca7876b969ace9491bbaf1e3f
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-10 17:52:07 +0200

    Move parts of app.js to bin/www

--- app.js
+++ app.js
@@ -4,21 +4,42 @@
 var express = require('express');
 var models = require('./models');
 
-var port = 3000;
 var app = express();
 
 // Routes
 var routes = require('./routes/index');
 app.use('/', routes);
 
-// Run server
-models
-	.sequelize
-	.sync()
-	.then(startApp);
 
-// Util
-function startApp() {
-	app.listen(port);
-	console.log('App started on port ' + port);
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
 }
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
