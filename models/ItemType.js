e842f6e28539abe76e44cdb30a72cd46bcaceb34
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-12 21:25:55 +0200

    Inline variable

--- models/ItemType.js
+++ models/ItemType.js
@@ -1,9 +1,7 @@
 'use strict';
 
 module.exports = function(sequelize, DataTypes) {
-	var ItemType = sequelize.define("ItemType", {
+	return sequelize.define("ItemType", {
 		name: DataTypes.STRING
 	});
-
-	return ItemType;
 };
