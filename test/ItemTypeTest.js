300054a6a090d597e4059061e466f768924605f2
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-12 21:08:51 +0200

    Pedal back a bit on test code

--- test/ItemTypeTest.js
+++ test/ItemTypeTest.js
@@ -4,20 +4,15 @@ var dbScaffolding = require('./DbScaffolding');
 module.exports = dbScaffolding;
 dbScaffolding.databaseExampleTest = {
     createAndVerifyItemType: function(test) {
-        createItemType({
+        models
+        .ItemType
+        .create({
             name: 'TestItemType42'
-        },
-        function verify(itemType) {
+        })
+        .then(function verify(itemType) {
             test.ok(itemType.id);
             test.equal(itemType.name, 'TestItemType42');
             test.done();
         });
     }
 };
-
-function createItemType(values, callback) {
-    models
-    .ItemType
-    .create(values)
-    .then(callback);
-}
