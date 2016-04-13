f38474223e75ec11a9d83a95495a806dc9d80536
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-13 19:59:57 +0200

    Model items in shopping list and start adding associations

--- models/ShoppingList.js
+++ models/ShoppingList.js
@@ -3,5 +3,13 @@
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define("ShoppingList", {
         name: DataTypes.STRING
+    },{
+        classMethods: {
+            associate: function(models) {
+                models
+                .ShoppingList
+                .hasMany(models.ShoppingListItem, {foreignKey: 'shoppingListId'});
+            }
+        }
     });
 };
--- test/ShoppingListModelTest.js
+++ test/ShoppingListModelTest.js
@@ -3,6 +3,18 @@ var dbScaffolding = require('./DbScaffolding');
 
 module.exports = dbScaffolding;
 dbScaffolding.shoppingListDbTest = {
+    setUp: function(callback) {
+        var self = this;
+        models
+        .ItemType
+        .create({
+            name: 'TestItemType'
+        })
+        .then(function (itemType) {
+            self.itemType = itemType;
+            callback();
+        });
+    },
     createEmptyShoppingList: function(test) {
         models
         .ShoppingList
@@ -12,6 +24,28 @@ dbScaffolding.shoppingListDbTest = {
         .then(function verify(shoppingList) {
             test.ok(shoppingList.id);
             test.equal(shoppingList.name, 'TestShoppingList');
+            test.done();
+        });
+    },
+    addItemToShoppingList: function(test) {
+        var self = this;
+
+        models
+        .ShoppingList
+        .create({
+            name: 'ShoppingList'
+        })
+        .then(function(shoppingList) {
+            return models
+            .ShoppingListItem
+            .create({
+                shoppingListId: shoppingList.id,
+                itemType: self.itemType,
+                quantity: '2'
+            })
+        })
+        .then(function(shoppingListItem) {
+            test.ok(shoppingListItem.id);
             test.done();
         });
     }
