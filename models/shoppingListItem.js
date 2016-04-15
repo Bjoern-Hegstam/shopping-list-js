0d6b5a1fa864950d4822df2469059bd18a559d77
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-15 17:41:52 +0200

    Fix relation between shoppingListItem and itemType

--- models/shoppingListItem.js
+++ models/shoppingListItem.js
@@ -6,7 +6,7 @@ module.exports = function(sequelize, DataTypes) {
     },{
         classMethods: {
             associate: function(models) {
-                ShoppingListItem.hasOne(models.itemType);
+                ShoppingListItem.belongsTo(models.itemType);
                 ShoppingListItem.belongsTo(models.shoppingList);
             }
         }
