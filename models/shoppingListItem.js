1473a9a027508210b86fa46382224d6ffea69933
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-17 13:34:37 +0200

    Add inCart-attribute for list items

--- models/shoppingListItem.js
+++ models/shoppingListItem.js
@@ -2,7 +2,8 @@
 
 module.exports = function(sequelize, DataTypes) {
     var ShoppingListItem = sequelize.define("shoppingListItem", {
-        quantity: DataTypes.INTEGER
+        quantity: DataTypes.INTEGER,
+        inCart: DataTypes.BOOLEAN
     },{
         classMethods: {
             associate: function(models) {
