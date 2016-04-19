6e2fa966195b80d0953dab4129fb1ed5c458ee54
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-19 22:02:03 +0200

    Start on shopping list template

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,38 @@ var models = require("../models");
 var express = require("express");
 var router = express.Router();
 
+var ShoppingList = models.shoppingList;
+
 var responseFormatter = require("./responseFormatter.js");
 
-router.get("/", function(req, res) {
-    models
-    .shoppingList
-    .findAll()
-    .then(shopppingLists => {
-        res.render('home', responseFormatter.formatCollectionResponse('', shopppingLists));
-    });
+router.get("/", (req, res) => {
+    ShoppingList
+        .findAll()
+        .then(shopppingLists => {
+            res.render('home', responseFormatter.formatCollectionResponse('', shopppingLists));
+        });
 
+});
+
+router.get("/shopping_list/:id", (req, res) => {
+    var respData;
+
+    //TODO: Need to get all used ItemTypes so we can show their names
+
+    ShoppingList
+        .findById(req.params.id)
+        .then(shoppingList => {
+            respData = responseFormatter.formatSingleItemResponse('', shoppingList);
+
+            return shoppingList.getShoppingListItems();
+        })
+        .then(items => {
+            respData.included = responseFormatter
+                .formatCollectionResponse('', items)
+                .data;
+
+            res.render("shopping_list", respData);
+        });
 });
 
 module.exports = router;
