66e40b455f428c0bec04660fe7f8c2cdd817d28b
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-19 21:09:48 +0200

    Experiment with handlebars

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,16 @@ var models = require("../models");
 var express = require("express");
 var router = express.Router();
 
+var responseFormatter = require("./responseFormatter.js");
+
 router.get("/", function(req, res) {
-    res.render('home');
+    models
+    .shoppingList
+    .findAll()
+    .then(shopppingLists => {
+        res.render('home', responseFormatter.formatCollectionResponse('', shopppingLists));
+    });
+
 });
 
 module.exports = router;
