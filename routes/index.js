7312f17d355071ca48706d4b2e70333319df391b
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-20 19:51:57 +0200

    Remove extra semicolon

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ var ShoppingListItem = models.shoppingListItem;
 var ItemType = models.itemType;
 
 var modelAttributeMapper = require('./modelAttributeMapper.js');
+var responseFormatter = require("./responseFormatter.js");
 
 router.get("/", (req, res) => {
     ShoppingList
@@ -38,14 +39,14 @@ router.get("/shopping_list/:id", (req, res) => {
         })
         .then(items => {
             respData.shopping_list.items = items.map(item => {
-                var itemData = modelAttributeMapper[ShoppingListItem.name](item);;
+                var itemData = modelAttributeMapper[ShoppingListItem.name](item);
                 itemData.item_type = modelAttributeMapper[ItemType.name](item.itemType);
 
                 return itemData;
             });
 
             res.render("shopping_list", respData);
-        })
+        });
 });
 
 module.exports = router;
