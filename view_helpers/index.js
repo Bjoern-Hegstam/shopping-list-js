fef00d932c5b1dcf2e0c280315e4b222ab76c706
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-04-30 12:48:28 +0200

    Start on admin menu

--- view_helpers/index.js
+++ view_helpers/index.js
@@ -1,3 +1,10 @@
 exports.toggleClass = (val, classTrue, classFalse) => {
     return val === 'true' ? classTrue : classFalse;
 };
+
+exports.isAdmin = (user, options) => {
+    if (user.role == 'ADMIN') {
+        return options.fn(user);
+    }
+    return options.inverse(user);
+};
