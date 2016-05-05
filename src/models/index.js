26fd276e28e91ad61a09b26765d07dfc7c4d275b
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-05-05 21:06:33 +0200

    Generate new model loader via sequelize-cli

--- src/models/index.js
+++ src/models/index.js
@@ -3,49 +3,38 @@
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
-const env = process.env.NODE_ENV || 'development';
+
 const debug = require('debug')('models:index');
-const config = require('./../../config/config.json')[env];
+const basename  = path.basename(module.filename);
+const env       = process.env.NODE_ENV || 'development';
+const config    = require('./../../config/config.json')[env];
+const db        = {};
 
+debug('db config:');
+debug(JSON.stringify(config, null, 2));
 
-let sequelize;
-if (process.env.DATABASE_URL) {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres',
-        protocol: 'postgres',
-        logging: false
-    });
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
-    console.log('Loading db');
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-const db = {};
-
-debug('db config: ' + JSON.stringify(config, null, 2));
-
 fs
-    .readdirSync(__dirname)
-    .filter(isModelFile)
-    .forEach(loadModel);
+  .readdirSync(__dirname)
+  .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+  .forEach(file => {
+    debug('Loading: ' + file);
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
 
 Object.keys(db).forEach(modelName => {
-    if ('associate' in db[modelName]) {
-        db[modelName].associate(db);
-    }
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
 });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 export default db;
-
-function isModelFile(file) {
-    return (file.indexOf('.') !== 0) && (file !== 'index.js');
-}
-
-function loadModel(file) {
-    debug('Loading: ' + file);
-    const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-}
