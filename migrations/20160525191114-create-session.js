cedf7362391d317f2dd897e417ba4bbe486937e9
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-05-25 21:12:27 +0200

    Add Session table creation script

---
+++ migrations/20160525191114-create-session.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('Sessions', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            sid: {
+                type: Sequelize.STRING,
+                primaryKey: true
+            },
+            expires: Sequelize.DATE,
+            data: Sequelize.TEXT,
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('Sessions');
+    }
+};
