662247c361d58a4f8f220f43040e39b8836ad58a
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-05-25 21:00:11 +0200

    Add table creation script for Users-table

---
+++ migrations/20160525185451-create-user.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            username: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            email: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            verified: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false
+            },
+            role: {
+                type: Sequelize.ENUM('USER', 'ADMIN'),
+                allowNull: false
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('Users');
+    }
+};
