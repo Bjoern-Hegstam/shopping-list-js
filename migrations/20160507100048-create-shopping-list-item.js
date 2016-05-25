562359dae87bc1dd1dfac6bd39ae035ba82dfdce
Björn Hegstam <bjorn.hegstam@gmail.com>
2016-05-25 21:10:12 +0200

    Fix indentation of migration scripts

--- migrations/20160507100048-create-shopping-list-item.js
+++ migrations/20160507100048-create-shopping-list-item.js
@@ -1,33 +1,33 @@
 'use strict';
 module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('shoppingListItems', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      quantity: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      },
-      inCart: {
-        type: Sequelize.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('ShoppingListItems');
-  }
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('shoppingListItems', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            quantity: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            inCart: {
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+                defaultValue: false
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('ShoppingListItems');
+    }
 };
